62183c5dd56380e8591e4734f979fa0c780a0a7f
drewhingster <andrewhing19@gmail.com>
2026-03-06 14:24:49 -0400

    Bump service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Procurement Management System PWA
-const CACHE_NAME = 'procurement-v1';
+const CACHE_NAME = 'procurement-v2';
 const urlsToCache = [
     '/',
     '/index.html',

1c10b4aaab0456da3cc767d791511a47ad83df2e
drewhingster <andrewhing19@gmail.com>
2026-03-11 08:39:47 -0400

    Fix checkbox button - clean white empty box when unchecked, green with tick when checked

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Procurement Management System PWA
-const CACHE_NAME = 'procurement-v6';
+const CACHE_NAME = 'procurement-v7';
 const urlsToCache = [
     '/',
     '/index.html',

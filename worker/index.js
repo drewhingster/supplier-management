ba9afc6b978575e1d32069e3d7c78fea99005064
drewhingster <andrewhing19@gmail.com>
2026-04-02 14:29:27 -0400

    Add PATCH to CORS allowed methods for claim endpoint

--- worker/index.js
+++ worker/index.js
@@ -13,7 +13,7 @@
 
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Max-Age': '86400',
 };

83895fe9a5af884c788f1c0e98fee48b925abb59
drewhingster <andrewhing19@gmail.com>
2026-04-02 14:19:58 -0400

    Widen unclaimed popup and show full titles without truncation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Procurement Management System PWA
-const CACHE_NAME = 'procurement-v14';
+const CACHE_NAME = 'procurement-v15';
 const urlsToCache = [
     '/',
     '/index.html',

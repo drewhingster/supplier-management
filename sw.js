da483cee9f889bf4b6e1531a691a31e4055da2e9
drewhingster <andrewhing19@gmail.com>
2026-05-07 14:10:12 -0400

    Move contract checkbox outside single-contractor section so its always visible

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Procurement Management System PWA
-const CACHE_NAME = 'procurement-v17';
+const CACHE_NAME = 'procurement-v18';
 const urlsToCache = [
     '/',
     '/index.html',
